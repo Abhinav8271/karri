a7d00f815fb50a201d6acb6d169a2db9cf4db4c8
Abhinav8271 <kumarabhinav.gautam@gmail.com>
2026-01-08 16:43:39 +0545

    storing user in database

--- components/header.jsx
+++ components/header.jsx
@@ -23,8 +23,11 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { checkUser } from '@/lib/checkUser'
 
-const Header = () => {
+const Header = async() => {
+
+  checkUser();
   return (
 
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-backdrop-filter:bg-background/60">
